64ac6591b5202751469c18dff0de9e824551a5d4
Gotty <78111224+iGotty@users.noreply.github.com>
2023-10-18 22:30:46 -0500

    INSTALED APpS

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -20,6 +20,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('', include('measurements.urls')),
+    path('', include('activos.urls')),
     path('', include('variables.urls')),
+    path('health-check/', views.healthCheck),
 ]
