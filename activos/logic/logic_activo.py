fff1e0fa2fdef5b45d778814bdcfc027de656419
Gotty <78111224+iGotty@users.noreply.github.com>
2023-10-18 22:41:35 -0500

    Update logic_activo.py

--- activos/logic/logic_activo.py
+++ activos/logic/logic_activo.py
@@ -1,10 +1,10 @@
 from ..models import Activo
 
-def get_Activos():
+def get_activos():
     queryset = Activo.objects.all().order_by('-dateTime')[:10]
     return (queryset)
 
-def create_Activo(form):
-    Activo = form.save()
-    Activo.save()
+def create_activo(form):
+    activo = form.save()
+    activo.save()
     return ()
